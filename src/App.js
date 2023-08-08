719711f65143665b43e0051ee5502637077d960b
Медведев Иван <ranzar2000@gmail.com>
2023-08-08 16:55:10 +0300

    Add displaying coords for each move

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = 'O';
     }
-    onPlay(nextSquares);
+    onPlay(nextSquares, i);
   }
 
   const winner = calculateWinner(squares);
@@ -87,16 +87,17 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null), selectedSquare: null }]);
   const [currentMove, setCurrentMove] = useState(0);
   const [isAscending, setIsAscending] = useState(true);
-  const currentSquares = history[currentMove];
+  const currentStep = history[currentMove];
+  const currentSquares = currentStep.squares;
   const xIsNext = currentMove % 2 === 0;
 
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+  function handlePlay(nextSquares, selectedSquare) {
+    const nextHistory = [...history.slice(0, currentMove + 1), { squares: nextSquares, selectedSquare }];
     setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1)
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
@@ -105,15 +106,18 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+    let numberSquare = squares.selectedSquare;
+    const row = Math.floor(numberSquare / 3) + 1;
+    const col = numberSquare % 3 + 1;
     if (move > 0) {
-      description = "Go to move #" + move;
+      description = "Go to move #" + move + ' (' + row + ', ' + col + ')';
     } else if (move === 0) {
       description = "Go to game start";
     }
 
     if (move === currentMove) {
       if (move > 0) {
-        description = "You're at move #" + move;
+        description = "You're at move #" + move + ' (' + row + ', ' + col + ')';
       } else if (move === 0) {
         description = "Tap square to start!";
       }
